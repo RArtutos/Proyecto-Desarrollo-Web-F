f1328c8aedcaab95f20dc10288862c654646b213
Arturo Romero <116369556+RArtutos@users.noreply.github.com>
2026-01-15 11:19:52 -0600

    Remove sensitive credentials from config.php
    Cleared sensitive database and Telegram bot credentials.

--- app/config/config.php
+++ app/config/config.php
@@ -1,10 +1,10 @@
 <?php
-    $usuario = "artutos1_administrador";
-    $contrasena = "]FaNJoO9NF]ASR!J";
-    $servidor = "107.181.134.10";
-    $basededatos = "artutos1_monitor";
+    $usuario = "";
+    $contrasena = "";
+    $servidor = "";
+    $basededatos = "";
 
     //Configuración de Telegram
-    define('TELEGRAM_BOT_TOKEN', '8487533767:AAFP2ACaydMguds60n_DFId09Ztw2TMNmQA');
-    define('TELEGRAM_CHAT_ID', '-1003392021155');
+    define('TELEGRAM_BOT_TOKEN', '');
+    define('TELEGRAM_CHAT_ID', '');
 ?>
